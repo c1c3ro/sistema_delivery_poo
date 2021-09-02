2d88bdcdce8eef7114d5933d9d6d2a8bf6f838a6
Cicero <cicerosousa205@gmail.com>
2021-09-02 12:53:21 -0300

    a tela da sacola acabou com minha vida

--- codigo/src/application/ViewMenuClient.java
+++ codigo/src/application/ViewMenuClient.java
@@ -140,7 +140,8 @@ public class ViewMenuClient {
 
     @FXML
     void seeBag(ActionEvent event) {
-    	return;
+    	Main m = new Main();
+        m.changeScene("clientBag.fxml");
     }
     
     @FXML
